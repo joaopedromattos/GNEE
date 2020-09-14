de9533fe61d3ab0be608d137789f5443e4092708
João Pedro Rodrigues Mattos <joao_pedro_mattos@hotmail.com>
2020-09-14 18:05:53 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,32 @@
 import numpy as np
 import scipy.sparse as sp
 import torch
-
+import spektral as spk
 
 def encode_onehot(labels):
     classes = set(labels)
     classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
     labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
     return labels_onehot
+
+
+def load_data(path="./pyGAT/data/cora/", dataset="cora"):
+    if (dataset == 'cora'):
+        adj, features, labels, train, val, test = spk.datasets.citation.load_data(dataset_name='cora', normalize_features=True, random_split=True)
+
+        # Converting one-hot encoding into categorical 
+        # values with the indexes of each dataset partition
+        idx_train, idx_val, idx_test = np.where(train)[0], np.where(val)[0], np.where(test)[0]
+
+
+        adj = torch.FloatTensor(adj.todense())
+        features = torch.FloatTensor(features.todense())
+        labels = torch.LongTensor(np.where(labels)[1])
+        idx_train = torch.LongTensor(idx_train)
+        idx_val = torch.LongTensor(idx_val)
+        idx_test = torch.LongTensor(idx_test)
+
+        return adj, features, labels, idx_train, idx_val, idx_test
 
 
 def load_data(path="./pyGAT/data/cora/", dataset="cora"):
