235a55bcb28f5cd2363d0cf0ede8bf4881fbf6c8
João Pedro Mattos <joao_pedro_mattos@hotmail.com>
2020-09-17 12:13:17 -0300

    This commit ables us to choose which dataset to use in our benchmarks.
    By using spektral, we're able to rapidly adapt our code to benchmark many different datasets!

--- train.py
+++ train.py
@@ -13,22 +13,32 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 
-from utils import load_data, accuracy
+from utils import new_load_data, accuracy
 from models import GAT, SpGAT
 
 # Training settings
 parser = argparse.ArgumentParser()
-parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
-parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
-parser.add_argument('--sparse', action='store_true', default=False, help='GAT with sparse version or not.')
+parser.add_argument('--no-cuda', action='store_true',
+                    default=False, help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true',
+                    default=False, help='Validate during training pass.')
+parser.add_argument('--sparse', action='store_true',
+                    default=False, help='GAT with sparse version or not.')
 parser.add_argument('--seed', type=int, default=72, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train.')
-parser.add_argument('--lr', type=float, default=0.005, help='Initial learning rate.')
-parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
-parser.add_argument('--hidden', type=int, default=8, help='Number of hidden units.')
-parser.add_argument('--nb_heads', type=int, default=8, help='Number of head attentions.')
-parser.add_argument('--dropout', type=float, default=0.6, help='Dropout rate (1 - keep probability).')
-parser.add_argument('--alpha', type=float, default=0.2, help='Alpha for the leaky_relu.')
+parser.add_argument('--epochs', type=int, default=10000,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.005,
+                    help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=8,
+                    help='Number of hidden units.')
+parser.add_argument('--nb_heads', type=int, default=8,
+                    help='Number of head attentions.')
+parser.add_argument('--dropout', type=float, default=0.6,
+                    help='Dropout rate (1 - keep probability).')
+parser.add_argument('--alpha', type=float, default=0.2,
+                    help='Alpha for the leaky_relu.')
 parser.add_argument('--patience', type=int, default=100, help='Patience')
 
 args = parser.parse_args()
@@ -41,25 +51,25 @@ if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
 # Load data
-adj, features, labels, idx_train, idx_val, idx_test = load_data()
+adj, features, labels, idx_train, idx_val, idx_test = new_load_data()
 
 # Model and optimizer
 if args.sparse:
-    model = SpGAT(nfeat=features.shape[1], 
-                nhid=args.hidden, 
-                nclass=int(labels.max()) + 1, 
-                dropout=args.dropout, 
-                nheads=args.nb_heads, 
-                alpha=args.alpha)
+    model = SpGAT(nfeat=features.shape[1],
+                  nhid=args.hidden,
+                  nclass=int(labels.max()) + 1,
+                  dropout=args.dropout,
+                  nheads=args.nb_heads,
+                  alpha=args.alpha)
 else:
-    model = GAT(nfeat=features.shape[1], 
-                nhid=args.hidden, 
-                nclass=int(labels.max()) + 1, 
-                dropout=args.dropout, 
-                nheads=args.nb_heads, 
+    model = GAT(nfeat=features.shape[1],
+                nhid=args.hidden,
+                nclass=int(labels.max()) + 1,
+                dropout=args.dropout,
+                nheads=args.nb_heads,
                 alpha=args.alpha)
-optimizer = optim.Adam(model.parameters(), 
-                       lr=args.lr, 
+optimizer = optim.Adam(model.parameters(),
+                       lr=args.lr,
                        weight_decay=args.weight_decay)
 
 if args.cuda:
@@ -110,6 +120,7 @@ def compute_test():
     print("Test set results:",
           "loss= {:.4f}".format(loss_test.data[0]),
           "accuracy= {:.4f}".format(acc_test.data[0]))
+
 
 # Train model
 t_total = time.time()
--- utils.py
+++ utils.py
@@ -1,23 +1,32 @@
 import numpy as np
 import scipy.sparse as sp
 import torch
+import spektral as spk
 
 
 def encode_onehot(labels):
     classes = set(labels)
-    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
-    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+    classes_dict = {c: np.identity(len(classes))[
+        i, :] for i, c in enumerate(classes)}
+    labels_onehot = np.array(
+        list(map(classes_dict.get, labels)), dtype=np.int32)
     return labels_onehot
 
 
-def load_data(path="./pyGAT/data/cora/", dataset="cora"):
+def new_load_data(path="./pyGAT/data/cora/", dataset="cora"):
+    print(f"[LOAD DATA]: {dataset}")
     if (dataset == 'cora'):
-        adj, features, labels, train, val, test = spk.datasets.citation.load_data(dataset_name='cora', normalize_features=True, random_split=True)
+        adj, features, labels, train, val, test = spk.datasets.citation.load_data(
+            dataset_name='cora', normalize_features=True, random_split=True)
 
-        # Converting one-hot encoding into categorical 
+        # Converting one-hot encoding into categorical
         # values with the indexes of each dataset partition
-        idx_train, idx_val, idx_test = np.where(train)[0], np.where(val)[0], np.where(test)[0]
+        idx_train, idx_val, idx_test = np.where(train)[0], np.where(val)[
+            0], np.where(test)[0]
 
+        # Normalizing our features and adjacency matrices
+        features = normalize_features(features)
+        adj = normalize_adj(adj + sp.eye(adj.shape[0]))
 
         adj = torch.FloatTensor(adj.todense())
         features = torch.FloatTensor(features.todense())
@@ -28,24 +37,30 @@ def load_data(path="./pyGAT/data/cora/", dataset="cora"):
 
         return adj, features, labels, idx_train, idx_val, idx_test
 
-def load_data_original(path="./data/cora/", dataset="cora"):
-    """Load citation network dataset (cora only for now)"""
-    print('Loading {} dataset...'.format(dataset))
 
-    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+def load_data(path="./pyGAT/data/cora/", dataset="cora"):
+    """Load citation network dataset (cora only for now)"""
+    print('Test {} dataset...'.format(dataset))
+
+    idx_features_labels = np.genfromtxt(
+        "{}{}.content".format(path, dataset), dtype=np.dtype(str))
     features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
     labels = encode_onehot(idx_features_labels[:, -1])
 
     # build graph
     idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
     idx_map = {j: i for i, j in enumerate(idx)}
-    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset), dtype=np.int32)
-    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())), dtype=np.int32).reshape(edges_unordered.shape)
-    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])), shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+    edges_unordered = np.genfromtxt(
+        "{}{}.cites".format(path, dataset), dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])), shape=(
+        labels.shape[0], labels.shape[0]), dtype=np.float32)
 
     # build symmetric adjacency matrix
     adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
 
+    # Normalizing our features and adjacency matrices
     features = normalize_features(features)
     adj = normalize_adj(adj + sp.eye(adj.shape[0]))
 
@@ -88,4 +103,3 @@ def accuracy(output, labels):
     correct = preds.eq(labels).double()
     correct = correct.sum()
     return correct / len(labels)
-
