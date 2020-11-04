bc201e756ba008e74984db68cbd5b91475fc2c3c
João Pedro Mattos <joao_pedro_mattos@hotmail.com>
2020-11-04 15:08:00 -0300

    Fixing minor issue with compute test method

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ class GAT_wrapper():
         self.loss_test = loss_test
         self.acc_test = acc_test
 
-        return loss_test, acc_test, output[self.idx_test]
+        return loss_test, acc_test, output[self.idx_test].max(1)[1]
 
     def train_pipeline(self, *args, custom_function=True, function=None):
         random.seed(self.args.seed)
